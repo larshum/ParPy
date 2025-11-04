6b18dafcf9ec5e30c43acded70e4b28c70423351
Lars Hummelgren <larshum@kth.se>
2025-11-04 14:05:33 +0100

    Update version to 0.3.1

--- python/parpy/__init__.py
+++ python/parpy/__init__.py
@@ -11,4 +11,4 @@ from .buffer import sync
 from .main import threads, par_reduction, clear_cache, compile_string, print_compiled, callback, external, jit
 from .builtin import gpu, label
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
